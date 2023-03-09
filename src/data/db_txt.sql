3e0804c4a91c42d82842da94bcc0b1b69c330662
Jasper-Duong <duongngochung13@gmail.com>
2023-03-09 22:31:29 +0700

    Update database queries (add job & user)

--- src/data/db_txt.sql
+++ src/data/db_txt.sql
@@ -4,25 +4,25 @@
 -- DROP TABLE service;
 -- DROP TABLE job_subcategory;
 -- DROP TABLE job_category;
-CREATE TABLE job_category(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255));
-INSERT INTO job_category (`name`)
-		VALUES('Graphics & Design');
-INSERT INTO job_category (`name`)
-		VALUES('Digital Marketing');
-INSERT INTO job_category (`name`)
-		VALUES('Writing & Translation');
-INSERT INTO job_category (`name`)
-		VALUES('Video & Animation');
-INSERT INTO job_category (`name`)
-		VALUES('Music & Audio');
-INSERT INTO job_category (`name`)
-		VALUES('Programming & Tech');
-INSERT INTO job_category (`name`)
-		VALUES('Business');
-INSERT INTO job_category (`name`)
-		VALUES('Lifestyle');
-INSERT INTO job_category (`name`)
-		VALUES('Data');
+CREATE TABLE job_category(id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), iconUrl VARCHAR(3000));
+INSERT INTO job_category
+		VALUES(0, 'Graphics & Design', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/graphics-design.d32a2f8.svg");
+INSERT INTO job_category
+		VALUES(0, 'Digital Marketing', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/online-marketing.74e221b.svg");
+INSERT INTO job_category
+		VALUES(0, 'Writing & Translation', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/writing-translation.32ebe2e.svg");
+INSERT INTO job_category
+		VALUES(0, 'Video & Animation', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/video-animation.f0d9d71.svg");
+INSERT INTO job_category
+		VALUES(0, 'Music & Audio', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/music-audio.320af20.svg");
+INSERT INTO job_category
+		VALUES(0, 'Programming & Tech', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/programming.9362366.svg");
+INSERT INTO job_category
+		VALUES(0, 'Business', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/business.bbdf319.svg");
+INSERT INTO job_category
+		VALUES(0, 'Lifestyle', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/lifestyle.745b575.svg");
+INSERT INTO job_category
+		VALUES(0, 'Data', "https://fiverr-res.cloudinary.com/npm-assets/@fiverr/logged_out_homepage_perseus/apps/data.718910f.svg");
 
 CREATE TABLE job_subcategory (
 	id INT PRIMARY KEY AUTO_INCREMENT,
