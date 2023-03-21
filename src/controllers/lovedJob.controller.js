a2702d03b8ad521340f00ea9af0e9fb2e8de0e62
Jasper-Duong <duongngochung13@gmail.com>
2023-03-21 23:51:19 +0700

    Add api for add, remove & get lovedJob based on (userId, jobId)

---
+++ src/controllers/lovedJob.controller.js
@@ -0,0 +1,58 @@
+const prisma = require(".");
+const { failCode, successCode } = require("../utils/responseCode");
+
+async function addLovedJob(req, res) {
+  const { jobId, userId } = req.body;
+  const submitData = { job_id: +jobId, user_id: +userId };
+  console.log({ submitData });
+  try {
+    const data = await prisma.loved_job.create({
+      data: submitData,
+    });
+    return successCode(
+      res,
+      data,
+      `User ${userId} added job ${jobId} to their favorite list`
+    );
+  } catch (error) {
+    console.log(error);
+    failCode(res, null, "Failed to add job to favorite list");
+  }
+}
+
+async function getLovedJob(req, res) {
+  const { jobId, userId } = req.query;
+  console.log({ jobId, userId });
+  try {
+    const data = await prisma.loved_job.findUnique({
+      where: { user_id_job_id: { job_id: +jobId, user_id: +userId } },
+    });
+    return successCode(
+      res,
+      data,
+      `User ${userId} added job ${jobId} to their favorite list`
+    );
+  } catch (error) {
+    console.log(error);
+    return failCode(
+      res,
+      null,
+      `User ${userId} failed to add job ${jobId} to their favorite list`
+    );
+  }
+}
+
+async function removeLovedJob(req, res) {
+  const { jobId, userId } = req.body;
+  try {
+    const data = await prisma.loved_job.delete({
+      where: { user_id_job_id: { job_id: +jobId, user_id: +userId } },
+    });
+    return successCode(res, data, `User ${userId} removed job ${jobId} from their favorite list`)
+  } catch (error) {
+    console.log(error);
+    return failCode(res, null, `User ${userId} failed to remove job ${jobId} to their favorite list`)
+  }
+}
+
+module.exports = { addLovedJob, getLovedJob, removeLovedJob };
--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const { getUsers, getUserById } = require("../controllers/user.controller");
 const userRoute = Router();
 
-userRoute.use("/get-users", getUsers);
-userRoute.use("/get-user-by-id/:id", getUserById);
+userRoute.get("/get-users", getUsers);
+userRoute.get("/get-user-by-id/:id", getUserById);
 
 module.exports = userRoute;
