fdce7c304d824a25dc183e85ae8c236f6f5fa3bd
Jasper-Duong <duongngochung13@gmail.com>
2023-03-24 23:11:23 +0700

    Add auth -> check email & username existed

--- src/routes/Auth/auth.route.js
+++ src/routes/Auth/auth.route.js
@@ -1,5 +1,10 @@
 const { Router } = require("express");
-const { signIn, signUp } = require("../../controllers/Auth/auth.controller");
+const {
+  signIn,
+  signUp,
+  checkEmailExisted,
+  checkUsernameExisted,
+} = require("../../controllers/Auth/auth.controller");
 const {
   isUserInputEmptyMdw,
   validateSignUp,
@@ -7,6 +12,8 @@ const {
 } = require("../../middlewares/auth.mid");
 const authRoute = Router();
 
+authRoute.post("/check-email-existed", checkEmailExisted);
+authRoute.post("/check-username-existed", checkUsernameExisted);
 authRoute.post("/sign-up", isUserInputEmptyMdw, validateSignUp, signUp);
 authRoute.post("/sign-in", validateSignIn, signIn);
 
