cfea20eeaef1e2e556ce474dd36794c3cc4209ce
Jasper-Duong <duongngochung13@gmail.com>
2023-03-24 23:03:58 +0700

    add new user -> user TABLE in MySQL

--- src/data/user.sql
+++ src/data/user.sql
@@ -11,8 +11,8 @@ CREATE TABLE user (
   	avatar VARCHAR(3000),
   	role VARCHAR(255),
 	CONSTRAINT UC_User UNIQUE (email, name));
-INSERT INTO user VALUES (0, "j_duong", "J Duong","jduong@gmail.com", "$2a$10$z/.XUYhZn988kWDeLq59COun6W.tisyTKtLY693nzcVhzok.08Kei", "2000-12-03", "Vietnam", 0,CURRENT_TIMESTAMP, "https://i.pravatar.cc/300?img=3", "admin");
-INSERT INTO user VALUES (1, "admin", "Admin","fiverr_admin@jopmail.com", "$2a$10$z/.XUYhZn988kWDeLq59COun6W.tisyTKtLY693nzcVhzok.08Kei", "2000-12-03", "Vietnam", 1,CURRENT_TIMESTAMP, "http://sc04.alicdn.com/kf/Hc3e61591078043e09dba7808a6be5d21n.jpg", "admin");
+INSERT INTO user VALUES (0, "j_duong", "J Duong","jduong@gmail.com", "$2a$10$ynlcAoleL1yQjznQNZevdeZ3aPel.T44gDJtsC9U7Hd4iPR/mZEh6", "2000-12-03", "Vietnam", 0, CURRENT_TIMESTAMP, "https://i.pravatar.cc/300?img=3", "admin");
+INSERT INTO user VALUES (0, "admin_fiverr", "Admin","admin_fiverr@jopmail.com", "$2a$10$w5ytAPEOv5KEzYSzqXBo1ei1FFthwk2/P0IKn5MjIfKeZC5gZMEW6", "2006-01-05", "United States", 0, CURRENT_TIMESTAMP, "http://sc04.alicdn.com/kf/Hc3e61591078043e09dba7808a6be5d21n.jpg", "admin");
 
 CREATE TABLE job (
 	id INT PRIMARY KEY AUTO_INCREMENT,
