06b27573259c9bb87dc608e12d5348fb17fe9900
Jasper-Duong <duongngochung13@gmail.com>
2023-03-26 23:29:36 +0700

    Add getJobByUser

--- src/routes/job.route.js
+++ src/routes/job.route.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
-const { getJobs, getJobById } = require("../controllers/job.controller");
+const {
+  getJobs,
+  getJobById,
+  getJobByUser,
+} = require("../controllers/job.controller");
 const {
   addLovedJob,
   getLovedJob,
@@ -11,10 +15,15 @@ const jobRoute = Router();
 
 jobRoute.get("/get-jobs", getJobs);
 jobRoute.get("/get-job-by-id/:id", getJobById);
+jobRoute.get("/get-jobs-by-user", getJobByUser);
 
 jobRoute.post("/add-loved-job", authenticateMdw, addLovedJob);
 jobRoute.delete("/remove-loved-job", authenticateMdw, removeLovedJob);
 jobRoute.get("/get-loved-job", authenticateMdw, getLovedJob);
-jobRoute.get("/get-loved-job-by-user-id", authenticateMdw, getLovedJobByUserId);
+jobRoute.get(
+  "/get-loved-job-by-user-id/:id",
+  authenticateMdw,
+  getLovedJobByUserId
+);
 
 module.exports = jobRoute;
